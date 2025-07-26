0472891e919efc726e6510f5be8fff3ccdf3ee9a
darina-bigos <darinaapp358@gmail.com>
2025-07-26 20:07:53 +0200

    up css

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -2,9 +2,9 @@ import css from "./CafeInfo.module.css";
 
 export default function Cafeinfo() {
   return (
-    <div className="css.container">
-      <h1 className="css.title">Sip Happens Café</h1>
-      <p className="css.description">
+    <div className={css.container}>
+      <h1 className={css.title}>Sip Happens Café</h1>
+      <p className={css.description}>
         Please rate our service by selecting one of the options below.
       </p>
     </div>
